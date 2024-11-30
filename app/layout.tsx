1216effdebaa39933d48476b85a7ffdb97c0d4cb
aniiaham <aniya_axm@icloud.com>
2024-11-29 21:44:18 -0600

    wip blogs

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={cn(
           `bg-background min-h-screen font-sans antialiased ${geistSans.variable} ${geistMono.variable} `
--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -2,7 +2,6 @@
 
 import { siteConfig } from "@/config/site";
 import Link from "next/link";
-import { Icons } from "./icons";
 import { cn } from "@/lib/utils";
 import { usePathname } from "next/navigation";
 
@@ -11,7 +10,6 @@ export function MainNav() {
   return (
     <nav className="flex items-center space-x-4 lg:space-x-6">
       <Link href="/" className="ml-6 flex items-center space-x-2">
-        <Icons.logo className="h-6 w-6" />
         <span className="font-bold">{siteConfig.name}</span>
       </Link>
       <Link
