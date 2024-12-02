e54f5e93c89dc82593496f83fbf577ed87b9a9ea
aniiaham <aniya_axm@icloud.com>
2024-12-02 14:22:11 -0600

    mdx blogs

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,10 +1,4 @@
-import { posts } from "#site/content";
-import { PostItem } from "@/components/post-item";
-import { sortPosts } from "@/lib/utils";
-
 export default async function BlogPage() {
-  const sortedPosts = sortPosts(posts.filter((post) => post.published));
-  const displayPosts = sortedPosts;
   return (
     <div className="container max-w-4xl py-6 lg:py-10">
       <div className="flex flex-col items-start gap-4 md:flex-row md:justify-between md:gap-8">
@@ -17,25 +11,7 @@ export default async function BlogPage() {
         </div>
       </div>
       <hr className="mt-8" />
-      {displayPosts?.length > 0 ? (
-        <ul className="flex flex-col">
-          {displayPosts.map((post) => {
-            const { slug, date, title, description } = post;
-            return (
-              <li key={slug}>
-                <PostItem
-                  slug={slug}
-                  date={date}
-                  title={title}
-                  description={description}
-                />
-              </li>
-            );
-          })}
-        </ul>
-      ) : (
-        <p>Nothing to see here yet</p>
-      )}
+      
     </div>
   );
 }
