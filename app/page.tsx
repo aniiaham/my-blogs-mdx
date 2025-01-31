0060688365ebf3f0f2562d515451f23e60ab706c
aniiaham <aniya_axm@icloud.com>
2025-01-30 22:10:02 -0600

    small changes blog post

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
 import Image from "next/image";
 import { NameTransition } from "./name";
 import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
 
 export default function Home() {
   return (
     <section className="mx-auto my-32 container flex flex-col items-center justify-center gap-4">
       <div className="text-lg font-bold font-mono">
-        <Link href={"https://github.com/aniiaham"} target="_blank">
+        <Link href={"https://aniia.dev/"} target="_blank">
           <NameTransition />
         </Link>
       </div>
@@ -26,6 +29,20 @@ export default function Home() {
           that function smoothly. My stack - JavaScript, TypeScript, React,
           Next.js, Node.js, and Tailwind CSS.
         </p>
+      </div>
+      <div className="border border-b-gray-300  max-w-lg w-full mt-12"></div>
+      <hr className="mt-12" />
+      <div className="flex flex-col justify-center">
+        <p className="text-lg font-bold">​​React Query как State Manager</p>
+        <Link
+          href={"/n/state-manager"}
+          className={cn(
+            buttonVariants({ variant: "link" }),
+            "py-0 text-sm sm:text-base font-medium hover:no-underline"
+          )}
+        >
+          Read more <ArrowRight className="w-2 h-2" />
+        </Link>
       </div>
     </section>
   );
