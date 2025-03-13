2d68ae498543e03439e616f410469a05bc14f1d3
aniiaham <aniya_axm@icloud.com>
2025-03-12 20:57:59 -0500

    added image

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 export default function BlogPage() {
   return (
-    <div className="container max-w-4xl py-6 lg:py-10 mx-auto my-4">
+    <div className="container max-w-4xl py-6 lg:py-10 mx-auto my-4 w-full">
       <div className="flex flex-col items-start gap-4 md:flex-row md:justify-between md:gap-8">
         <div className="flex-1 space-y-4">
           <h1 className="inline-block font-black text-4xl lg:text-5xl">
@@ -18,19 +18,19 @@ export default function BlogPage() {
         </div>
       </div>
       <hr className="mt-8" />
-      <div className="flex justify-between items-center mt-4">
-        <p className="text-2xl font-bold">​​React Query как State Manager</p>
-        <div className="border border-gray-200 rounded hover:border-gray-400">
-          <Link
-            href={"/n/state-manager"}
-            className={cn(
-              buttonVariants({ variant: "link" }),
-              "py-0 text-sm sm:text-base font-medium hover:no-underline"
-            )}
-          >
+      <div className="flex items-center mt-4 w-full">
+        <Link
+          href={"/n/state-manager"}
+          className={cn(
+            buttonVariants({ variant: "link" }),
+            "py-0 text-sm sm:text-base font-medium hover:no-underline"
+          )}
+        >
+          <p className="text-2xl font-bold">​​React Query как State Manager</p>
+          <div className="hover:border-gray-400 flex flex-row items-center gap-2">
             Read more <ArrowRight className="w-2 h-2" />
-          </Link>
-        </div>
+          </div>
+        </Link>
       </div>
     </div>
   );
